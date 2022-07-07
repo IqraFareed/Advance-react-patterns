a9765c1838d199ca2e052fac85fe1f4f5faff2b3
IqraFareed <iqrafareed56@gmail.com>
2022-07-07 16:16:11 +0500

    http request with use reducer

---
+++ src/Components/DataFetchingTwo.js
@@ -0,0 +1,51 @@
+import React , {useReducer , useEffect} from 'react'
+import axios from 'axios';
+
+const initialState ={
+    loading: true ,
+    error:"",
+    post:{}
+}
+
+const reducer = (state , action)=>{
+    switch(action.type){
+        case 'FETCH_SUCCESS':
+           return { 
+            loading: false,
+            post:action.payload,
+            error:""
+        }
+        case 'FETCH_ERROR':
+            return { 
+             loading: false,
+             post:{},
+             error:action.payload
+         }
+    default:
+        return state;
+    }
+}
+
+const DataFetchingTwo = () => {
+ const [state , dispatch]  = useReducer(reducer  ,initialState)
+ 
+ useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+       dispatch({type:"FETCH_SUCCESS" , payload:response.data})
+    })
+    .catch(error =>{
+        console.log(error)
+      dispatch({type:"FETCH_ERROR" , payload:error.message})
+    })
+  },[])
+ 
+ return (
+    <div>
+    {state.loading ? 'Loading' : state.post.title}
+    {state.error ? state.error : null}
+   </div>
+  )
+}
+
+export default DataFetchingTwo;
